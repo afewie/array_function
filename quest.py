95e76dc9a699267c9a8cf7097611651824e3d311
afewie <rabadanovrsh.2009@gmail.com>
2025-03-06 18:15:06 +0300

    Функция, возвращающая массив элементов, равные числу value

--- quest.py
+++ quest.py
@@ -6,3 +6,10 @@ def filter_greater(arr, value):
             new_lst.append(elem)
     return new_lst
 
+def filter_less(arr, value):
+    new_lst = []
+    
+    for elem in arr:
+        if elem < value:
+            new_lst.append(elem)
+    return new_lst
