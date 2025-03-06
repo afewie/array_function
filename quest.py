f4310d01e6fec442ee6c80dac686045177adeabc
afewie <rabadanovrsh.2009@gmail.com>
2025-03-06 18:15:47 +0300

    Функция, возвращающая массив элементов, неравные числу value

--- quest.py
+++ quest.py
@@ -13,3 +13,11 @@ def filter_less(arr, value):
         if elem < value:
             new_lst.append(elem)
     return new_lst
+
+def filter_equal(arr, value):
+    new_lst = []
+    
+    for elem in arr:
+        if elem == value:
+            new_lst.append(elem)
+    return new_lst
